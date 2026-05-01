707e442d765c73f96bc9a6775bdd94f831f79daa
lxrzlyr <1289539524@qq.com>
2026-05-02 02:07:12 +0800

    Finalize Homebrew distribution and README assets

--- packaging/homebrew/mtop.rb
+++ packaging/homebrew/mtop.rb
@@ -2,7 +2,7 @@ class Mtop < Formula
   desc "Apple Silicon terminal monitor for macOS"
   homepage "https://github.com/lxrzlyr/mtop"
   url "https://github.com/lxrzlyr/mtop/releases/download/v1.0.0/mtop-1.0.0-source.tar.gz"
-  sha256 "79666219551ee5e300d19c32048d22a17b80c8ee5c8b315f762e1dff7eda26b2"
+  sha256 "be34e89a628c15c5a73c985aa683c24e3aca6e63eabb55aacae6f6afce0324cf"
   license "GPL-3.0-or-later"
 
   depends_on "cmake" => :build
